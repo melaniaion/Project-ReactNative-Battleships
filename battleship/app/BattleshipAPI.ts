9b4cd5263e7286d4a2ca3e2a88a0892b1671ba15
Melania Ion <melaniaion24@gmail.com>
2024-05-17 23:30:08 +0300

    Add BattleShipAPI.ts

---
+++ battleship/app/BattleshipAPI.ts
@@ -0,0 +1,90 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const API_URL = "http://163.172.177.98:8081";
+
+export const registerUser = async (email: string, password: string) => {
+    try {
+        const payload = JSON.stringify({ email, password });
+
+        const response = await fetch(`${API_URL}/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: payload,
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || "Error: Something went wrong!");
+        }
+
+        return await response.json();
+    } catch (registerError) {
+        throw registerError;
+    }
+};
+
+export const loginUser = async (email: string, password: string) => {
+    try {
+        const payload = JSON.stringify({ email, password });
+
+        const response = await fetch(`${API_URL}/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: payload,
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || "Error: Something went wrong!");
+        }
+
+        const data = await response.json();
+        await AsyncStorage.setItem("accessToken", data.accessToken);
+        return data.token;
+
+    } catch (loginError) {
+        throw loginError;
+    }
+};
+
+export const getUserDetails = async (): Promise<{
+    email: string;
+    currentlyGamesPlaying: number;
+    gamesLost: number;
+    gamesPlayed: number;
+    gamesWon: number;
+  }> => {
+    try {
+      const token = await AsyncStorage.getItem("accessToken");
+      if (!token) {
+        throw new Error("No access token found");
+      }
+  
+      const response = await fetch(`${API_URL}/user/details/me`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error("Failed to fetch user details");
+      }
+  
+      const data = await response.json();
+
+      return {
+        email: data.user.email,
+        currentlyGamesPlaying: data.currentlyGamesPlaying,
+        gamesLost: data.gamesLost,
+        gamesPlayed: data.gamesPlayed,
+        gamesWon: data.gamesWon,
+      };
+    } catch (error) {
+      throw error;
+    }
+};
