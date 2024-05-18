c82d94018a91d759c7111d4ee783394253422d45
Melania Ion <melaniaion24@gmail.com>
2024-05-18 20:57:55 +0300

    update index.tsx

--- battleship/app/index.tsx
+++ battleship/app/index.tsx
@@ -1,5 +1,3 @@
-// app/index.tsx
-
 import "react-native-gesture-handler";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -9,27 +7,46 @@ import LoginScreen from "./LoginScreen";
 import UserDetailsScreen from "./UserDetailsScreen";
 import LobbyScreen from "./LobbyScreen";
 import AllGamesScreen from "./AllGamesScreen";
+import GameDetailsScreen from "./GameDetailsScreen";
+import JoinGameScreen from "./JoinGameScreen";
+import CreateGameScreen from "./CreateGameScreen";
 
-const Stack = createStackNavigator();
+type RootStackParamList = {
+  Home: undefined;
+  Register: undefined;
+  Login: undefined;
+  UserProfile: undefined;
+  Lobby: undefined;
+  AllGames: undefined;
+  GameDetails: { gameId: string };
+  JoinGame: undefined;
+  CreateGame: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function Index() {
   return (
     <Stack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "#b19cd9", // Light purple for header background
-      },
-      headerTintColor: "#fff", // White color for header text
-      headerTitleStyle: {
-        fontWeight: "bold",
-      },
-    }}>
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#b19cd9", // Light purple for header background
+        },
+        headerTintColor: "#fff", // White color for header text
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen name = "Login" component={LoginScreen} />
-      <Stack.Screen name="UserProfile" component={UserDetailsScreen}/>
-      <Stack.Screen name="Lobby" component={LobbyScreen}/>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="UserProfile" component={UserDetailsScreen} />
+      <Stack.Screen name="Lobby" component={LobbyScreen} />
       <Stack.Screen name="AllGames" component={AllGamesScreen} />
+      <Stack.Screen name="GameDetails" component={GameDetailsScreen} />
+      <Stack.Screen name="JoinGame" component={JoinGameScreen} />
+      <Stack.Screen name="CreateGame" component={CreateGameScreen} />
     </Stack.Navigator>
   );
 }
